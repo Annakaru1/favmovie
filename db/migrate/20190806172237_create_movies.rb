0012d98976037b7a43b4c408005f136ed5b4575c
Annakaru1 <aruchty@value.net>
2019-08-11 20:58:21 +0000

    db rollback and migrate redo

--- db/migrate/20190806172237_create_movies.rb
+++ db/migrate/20190806172237_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.string :year
-      t.string :synopsis
+      t.text :synopsis
       t.timestamps
     end
   end
