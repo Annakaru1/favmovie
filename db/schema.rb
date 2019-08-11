dab0c3b2fe12fcb15b102e4f89b652e8dbccf36b
Annakaru1 <aruchty@value.net>
2019-08-11 20:46:07 +0000

    undo and redo migrate

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_172237) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "year"
+    t.string "synopsis"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
